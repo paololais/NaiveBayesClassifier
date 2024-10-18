32eb8c3a7b9880f10b9fbc7cdaefa7b85989e085
paololais <paolo.lais1@gmail.com>
2024-10-18 14:45:37 +0200

    first commit

---
+++ NaiveBayes.py
@@ -0,0 +1,108 @@
+import numpy as np 
+
+class  NaiveBayes:
+
+	def __init__(self):
+		"""
+			Attributes:
+				likelihoods: Likelihood of each feature per class
+				class_priors: Prior probabilities of classes 
+				pred_priors: Prior probabilities of features 
+				features: All features of dataset
+
+		"""
+		self.features = list
+		self.likelihoods = {}
+		self.class_priors = {}
+		self.pred_priors = {}
+		self.seen_feature_values = {}  # Store unique values of features seen during training
+
+		self.X_train = np.array
+		self.y_train = np.array
+		self.train_size = int
+		self.num_feats = int
+
+	def fit(self, X, y):
+
+		self.features = list(X.columns)
+		self.X_train = X
+		self.y_train = y
+		self.train_size = X.shape[0]
+		self.num_feats = X.shape[1]
+
+		for feature in self.features:
+			self.likelihoods[feature] = {}
+			self.pred_priors[feature] = {}
+			self.seen_feature_values[feature] = []
+   
+			for feat_val in np.unique(self.X_train[feature]):
+				self.pred_priors[feature].update({feat_val: 0})
+				self.seen_feature_values[feature].append(feat_val)  # Track seen values
+
+				for outcome in np.unique(self.y_train):
+					self.likelihoods[feature].update({str(feat_val) + '_' + str(outcome): 0})
+					self.class_priors.update({outcome: 0})
+
+		self._calc_class_prior()
+		self._calc_likelihoods()
+		self._calc_predictor_prior()
+
+	def _calc_class_prior(self):
+		""" P(c) - Prior Class Probability """
+
+		for outcome in np.unique(self.y_train):
+			outcome_count = sum(self.y_train == outcome)
+			self.class_priors[outcome] = outcome_count / self.train_size
+
+	def _calc_likelihoods(self):
+		""" P(x|c) - Likelihood """
+
+		for feature in self.features:
+
+			for outcome in np.unique(self.y_train):
+				outcome_count = sum(self.y_train == outcome)
+				feat_likelihood = self.X_train[feature][self.y_train[self.y_train == outcome].index.values.tolist()].value_counts().to_dict()
+
+				for feat_val, count in feat_likelihood.items():
+					self.likelihoods[feature][str(feat_val) + '_' + str(outcome)] = count / outcome_count
+
+	def _calc_predictor_prior(self):
+		""" P(x) """
+
+		for feature in self.features:
+			feat_vals = self.X_train[feature].value_counts().to_dict()
+
+			for feat_val, count in feat_vals.items():
+				self.pred_priors[feature][feat_val] = count/self.train_size
+
+
+	def predict(self, X):
+		""" Calculates Posterior probability P(c|x) """
+		results = []
+		X = np.array(X)
+  
+		for query in X:
+			skip_pattern = False
+			probs_outcome = {}
+			for outcome in np.unique(self.y_train):
+				prior = self.class_priors[outcome]
+				likelihood = 1
+    
+				for feat, feat_val in zip(self.features, query):
+					if feat_val not in self.seen_feature_values[feat]:  # Check if unseen feature value 
+						print(f"Warning: Skipping pattern due to unseen feature value '{feat_val}' for feature '{feat}'.")
+						skip_pattern = True
+						break
+            		
+					likelihood *= self.likelihoods[feat][str(feat_val) + '_' + str(outcome)]
+
+				if skip_pattern:
+					break  # Skip this pattern if unseen value encountered
+				posterior = (likelihood * prior)
+
+				probs_outcome[outcome] = posterior
+
+			result = max(probs_outcome, key = lambda x: probs_outcome[x])
+			results.append(result)
+
+		return np.array(results)
---
+++ main.py
@@ -0,0 +1,91 @@
+import pandas as pd
+from NaiveBayes import NaiveBayes
+from LaplaceSmoothing import LaplaceSmoothing
+import time
+
+def get_num_levels(df):
+    num_levels = []
+    for col in df.columns[:-1]:
+        num_levels.append(df[col].nunique())
+    return num_levels
+
+def pre_processing(df):
+    # Convert 'Windy' column to string for consistency
+    df['Windy'] = df['Windy'].astype(str)
+	
+    # Select 10 random rows -> train set
+    #random_state = int(time.time())
+    df_train = df.sample(n=10, random_state=42)
+
+    # test set
+    df_test = df.drop(df_train.index)
+    
+    # Ensure test set has same features as training set or one less
+    assert set(df_train.columns) == set(df_test.columns) or set(df_train.columns) == set(df_test.columns - 1), "Test set features do not match training set features."
+
+    # partition data into features and target
+    X_train = df_train.iloc[:,:-1]  # features
+    y_train = df_train.iloc[:, -1]   # target
+    
+    X_test = df_test.iloc[:,:-1]  # features
+    y_test = df_test.iloc[:, -1]   # target
+    
+    num_levels = get_num_levels(df)
+    
+    return X_train, X_test, y_train, y_test, num_levels
+
+def accuracy_score(y_true, y_pred):
+
+	"""	score = (y_true - y_pred) / len(y_true) """
+
+	return round(float(sum(y_pred == y_true))/float(len(y_true)) * 100 ,2)
+
+
+
+if __name__=='__main__':
+    
+    # preprocessing
+    df = pd.read_csv("weather.data", delim_whitespace=True)    
+    X_train, X_test, y_train, y_test, num_levels = pre_processing(df)
+    
+    # Naive Bayes classifier
+    nb_clf = NaiveBayes()
+    # Train the model -> calculate all probabilities
+    nb_clf.fit(X_train, y_train)  
+    
+    # Test model with the extracted test set (X_test, y_test)
+    predictions = nb_clf.predict(X_test)  # Get predictions on the test set
+
+    print("Naive Bayes Classifier:")
+    # Print predicted and actual label
+    for i, (true_label, predicted_label) in enumerate(zip(y_test, predictions), 1):
+        print(f"Test {i}: Actual label = {true_label}, Predicted label = {predicted_label}")
+   
+    # Calculate accuracy on the test set
+    test_accuracy = accuracy_score(y_test, predictions)
+
+    # Print results
+    print(f"Test Accuracy: {test_accuracy}%")
+    
+    
+    ########################################################################################
+    # Task 3: Make the classifier robust to missing data with Laplace (additive) smoothing #
+    ########################################################################################
+    
+    ls_clf = LaplaceSmoothing()
+    # Train the model -> calculate all probabilities
+    ls_clf.fit(X_train, y_train, num_levels) 
+    
+    # Test model with the extracted test set (X_test, y_test)
+    predictions = ls_clf.predict(X_test)  # Get predictions on the test set
+
+    print("\nAfter Laplace smoothing:")
+    # Print predicted and actual label
+    for i, (true_label, predicted_label) in enumerate(zip(y_test, predictions), 1):
+        print(f"Test {i}: Actual label = {true_label}, Predicted label = {predicted_label}")
+   
+    # Calculate accuracy on the test set
+    test_accuracy = accuracy_score(y_test, predictions)
+
+    # Print results
+    print(f"Test Accuracy: {test_accuracy}%")
